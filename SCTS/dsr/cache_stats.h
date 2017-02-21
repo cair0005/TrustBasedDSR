b861f8b77cf0d4eb55f682c63f00f37a55104362
cair0005 <cair0005@users.noreply.github.com>
2017-02-22 00:53:57 +0800

    Update cache_stats.h

--- SCTS/dsr/cache_stats.h
+++ SCTS/dsr/cache_stats.h
@@ -1,15 +1,3 @@
-/*
- * cache_stats.h
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
- 
 #ifndef __cache_stats__
 #define __cache_stats__
 
